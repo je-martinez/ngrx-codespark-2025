f6da1ac870eacb4add1f69b4cc448490e171869d
Jose E. Martinez <josemartinezchevez@gmail.com>
2025-03-26 12:34:49 -0600

    chore: improvements

--- src/app/components/checkout-summary/checkout-summary.component.ts
+++ src/app/components/checkout-summary/checkout-summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
+import { CartFacade } from '../../store/cart';
 
 @Component({
   selector: 'checkout-summary-component',
@@ -7,8 +8,13 @@ import { Component, input } from '@angular/core';
 })
 export class CheckoutSummaryComponent {
   total = input<number>(0);
+  cartFacade = inject(CartFacade);
 
   get formattedTotal() {
     return `$${this.total()?.toFixed(2)}`;
+  }
+
+  onClickCheckout() {
+    this.cartFacade.clearCart();
   }
 }
